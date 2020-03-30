91a62e390e5b25aae640e18ef3155cfad3f9c9ad
cyrielle23 <61283127+cyrielle23@users.noreply.github.com>
2020-03-30 14:17:50 +0200

    Update Mes essais pour exo 3.R

--- Mes essais pour exo 3.R
+++ Mes essais pour exo 3.R
@@ -34,3 +34,58 @@ for (j in 1:150){
 Sys.sleep(.7)
 }
 
+# Control seed
+set.seed(23)
+
+# Number of steps
+steps <- 150
+
+# Direction probability (i.e. all direction are equally likely)
+probs <- c(0.25, 0.5, 0.75)
+
+# Initial matrix
+step_direction <- matrix(0, steps+1, 2)
+
+# Start random walk
+for (i in seq(2, steps+1)){
+  # Draw a random number from U(0,1)
+  rn = runif(1)
+  
+  # Go right if rn \in [0,prob[1])
+  if (rn < probs[1]) {step_direction[i,1] = 1}
+  
+  # Go left if rn \in [probs[1], probs[2])
+  if (rn >= probs[1] && rn < probs[2]) {step_direction[i,1] = -1}
+  
+  # Go forward if rn \in [probs[2], probs[3])
+  if (rn >= probs[2] && rn < probs[3]) {step_direction[i,2] = 1}
+  
+  # Go backward if rn \in [probs[3],1]
+  if (rn >= probs[3]) {step_direction[i,2] = -1}
+}
+
+# Cumulative steps
+position = data.frame(x = cumsum(step_direction[, 1]), 
+                      y = cumsum(step_direction[, 2]))
+
+# Let's make a nice graph...
+# Graph parameters
+color = "blue"
+xlab = "X-position"
+ylab = "Y-position"
+pt_pch = 16
+pt.cex = 2
+hues = seq(15, 375, length = 3)
+pt_col = hcl(h = hues, l = 65, c = 100)[1:2]
+par(mar = c(5.1, 5.1, 1, 2.1))
+
+# Main plot
+plot(NA,xlim =c(-10,10), 
+     ylim =c(-10,10), 
+     xlab ="X Position", 
+     ylab ="Y Position", main = "Bacteria Motility")
+
+
+# Add grid
+grid(NULL, NULL, lty = 1, col = "grey95")
+
